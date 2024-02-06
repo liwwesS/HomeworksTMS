694b0fad0305517ab75d87b518397d82988729e0
lonea1one <ka.novik@yandex.by>
2024-02-06 16:27:25 +0300

    Full complete HW3

---
+++ HW3/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace HW3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Point1();
+			Point2();
+			Point3();
+			Point4();
+			Point5();
+		}
+
+		static void Point1()
+		{
+            Console.WriteLine("TASK 1.");
+            for (int i = 0; i < 100; i++)
+			{
+				if (i % 2 != 0)
+                    Console.Write($"{i} ");
+            }
+            Console.WriteLine("\n");
+        }
+
+		static void Point2()
+		{
+			Console.WriteLine("TASK 2.");
+			for (int i = 5; i > 0; i--)
+			{
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine("\n");
+        }
+
+		static void Point3()
+		{
+            while (true)
+			{
+				Console.Write("TASK 3. Введите любое целое положительно число: ");
+				bool isIntPositiveNumber = int.TryParse(Console.ReadLine(), out int userEnteredNumber) && userEnteredNumber >= 0;
+				if (isIntPositiveNumber)
+				{
+					int sumNumbersToUserEntered = 0;
+					for (int i = 0; i < userEnteredNumber; i++)
+					{
+						sumNumbersToUserEntered += i;
+					}
+					Console.WriteLine($"Сумма чисел от 1 до введенного Вами числа = {sumNumbersToUserEntered}\n");
+					return;
+				}
+				else
+					Console.WriteLine("Ошибка ввода!\n");
+			}
+        }
+
+		static void Point4()
+		{
+            Console.WriteLine("TASK 4.");
+            int iterator = 7;
+			while (iterator < 100)
+			{
+                Console.Write($"{iterator} ");
+				iterator += 7;
+            }
+            Console.WriteLine("\n");
+        }
+
+		static void Point5()
+		{
+			int[] firstArray = { 1, 2, 3, 4, 5 };
+			int[] secondArray = { 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("Массив 1:");
+            int averageOfFirstArray = PrintAndCalculateArray(firstArray);
+
+			Console.WriteLine("Массив 2:");
+			int averageOfSecondArray = PrintAndCalculateArray(secondArray);
+
+			if (averageOfFirstArray > averageOfSecondArray)
+                Console.WriteLine("Среднее арифметическое первого массива больше");
+			else if (averageOfFirstArray < averageOfSecondArray)
+                Console.WriteLine("Среднее арифметическое второго массива больше");
+			else if (averageOfFirstArray == averageOfSecondArray)
+                Console.WriteLine("Среднне арифмитеческое обоих массивов равны");
+        }
+
+		static int PrintAndCalculateArray(int[] array)
+		{
+			int sumOfArray = 0;
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.Write($"{array[i]} ");
+				sumOfArray += array[i];
+			}
+
+			int average = sumOfArray / array.Length;
+			Console.WriteLine($"\nСреднее арифметическое = {average}\n");
+
+			return average;
+		}
+
+	}
+}
---
+++ HW3Hard/Program.cs
@@ -0,0 +1,129 @@
+﻿namespace HW3Hard
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.Write("Введите размер матрицы: ");
+				bool isArray = int.TryParse(Console.ReadLine(), out int arraySize);
+
+				if (isArray)
+				{
+					int[,] matrix = GenerateMatrix(arraySize);
+					PrintMatrix(matrix);
+
+					bool exit = false;
+					while (!exit)
+					{
+						Console.WriteLine("\nНа выбор следующие действия:");
+						Console.WriteLine("1 - Найти количество положительных/отрицательных чисел");
+						Console.WriteLine("2 - Сортировка элементов матрицы построчно");
+						Console.WriteLine("3 - Инверсия элементов матрицы построчно");
+						Console.WriteLine("4 - Выход\n");
+                        Console.Write("Ваше действие: ");
+
+                        bool isNumberChoice = int.TryParse(Console.ReadLine(), out int choice) && choice > 0;
+
+						if (isNumberChoice)
+						{
+							switch (choice)
+							{
+								case 1:
+									CountPosNegOfMatrix(matrix);
+									break;
+								case 2:
+									SortMatrix(matrix);
+									PrintMatrix(matrix);
+									break;
+								case 3:
+									InvertMatrix(matrix);
+									PrintMatrix(matrix);
+									break;
+								case 4:
+									exit = true;
+									return;
+								default:
+									Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+									break;
+							}
+						}				
+					}
+				}
+				else
+                    Console.WriteLine("Ошибка ввода!\n"); ;
+            }
+		}
+
+		static int[,] GenerateMatrix(int sizeArray)
+		{
+			var random = new Random();
+			int[,] matrix = new int[sizeArray, sizeArray];
+			for (int i = 0; i < sizeArray; i++)
+			{
+				for (int j = 0; j < sizeArray; j++)
+				{
+					matrix[i, j] = random.Next(-25, 25);
+				}
+			}
+			return matrix;
+		}
+
+		static void PrintMatrix(int[,] matrix)
+		{
+			Console.WriteLine("Матрица:");
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					Console.Write($"{matrix[i, j]}\t");
+				}
+				Console.WriteLine();
+			}
+		}
+
+		static void CountPosNegOfMatrix(int[,] matrix)
+		{
+			int positive = 0, negative = 0;
+			foreach (int value in matrix)
+			{
+				if (value > 0) positive++;
+				else if (value < 0) negative++;
+			}
+			Console.WriteLine($"Положительных чисел: {positive}, Отрицательных чисел: {negative}");
+		}
+
+		static void SortMatrix(int[,] matrix)
+		{
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+				{
+					for (int k = 0; k < matrix.GetLength(1) - j - 1; k++)
+					{
+						if (matrix[i, k] > matrix[i, k + 1])
+						{
+							int temp = matrix[i, k];
+							matrix[i, k] = matrix[i, k + 1];
+							matrix[i, k + 1] = temp;
+						}
+					}
+				}
+			}
+		}
+
+		static void InvertMatrix(int[,] matrix)
+		{
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1) / 2; j++)
+				{
+					int temp = matrix[i, j];
+					matrix[i, j] = matrix[i, matrix.GetLength(1) - j - 1];
+					matrix[i, matrix.GetLength(1) - j - 1] = temp;
+				}
+			}
+		}
+	}
+}
